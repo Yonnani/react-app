a4a9333bd65b4f6a06c65aaec1bc675fbe71d321
Yonnani <iamai5206@gmail.com>
2020-07-31 00:33:21 +0900

    16.1. 이벤트 state props render 함수 실습

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome', //'read',
       subject: {
         title: 'WEB', sub: 'World Wide Web!'
+      },
+      welcome: {
+        title: 'Welcome', desc: 'Hello, React!!'
       },
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information'},
@@ -19,13 +23,21 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
